6d346dc563b8d23b5035b6b595b5d14c90ac95d3
mariamiqbal9251 <163730535+mariamiqbal9251@users.noreply.github.com>
2024-04-03 13:27:08 +0500

    word counter commit

---
+++ wc.js
@@ -0,0 +1,12 @@
+import inquirer from "inquirer";
+let paragraph = await inquirer.prompt([
+    {
+        type: "input",
+        name: "para",
+        message: " Enter your Paragraph \n"
+    }
+]);
+const characterCount = paragraph.para.length;
+console.log(" The total characters in your paragrapgh are \t" + characterCount);
+const wordCount = paragraph.para.split(/\s+/).length;
+console.log("The number of words in you paragraph are \t" + wordCount);
